473bd62f065f09c997a23048f1579133e2ac2fc1
John Rock <ivanshevchenko@mail.ru>
2019-12-10 17:44:19 +0200

    main page design activity

---
+++ app/src/main/java/com/webmaster/doctoeasy/MainActivity.java
@@ -0,0 +1,31 @@
+package com.webmaster.doctoeasy;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void OnClickLogin(View view) {
+
+               Intent intent =new Intent(MainActivity.this,LogInActivity.class);
+            startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/webmaster/doctoeasy/ServerApi.java
@@ -0,0 +1,23 @@
+package com.webmaster.doctoeasy;
+
+
+
+    public interface ServerApi {
+
+//        @GET("getProducts")
+//        List<Product> getProducts();
+//
+//        @GET("getProduct")
+//        Product getProduct(long productId);
+//
+//        @GET("getOrders")
+//        List<Order> getOrders();
+//
+//        @GET("getOrder")
+//        Order getOrder(long orderId);
+//
+//        @POST("createOrder")
+//        void createOrder(Order order);
+
+    }
+
