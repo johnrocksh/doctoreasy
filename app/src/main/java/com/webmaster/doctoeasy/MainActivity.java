5ee62c37aa1e8272d582cb24ad0aee81e2216c25
John Rock <ivanshevchenko@mail.ru>
2019-12-10 18:46:33 +0200

    firs page

--- app/src/main/java/com/webmaster/doctoeasy/MainActivity.java
+++ app/src/main/java/com/webmaster/doctoeasy/MainActivity.java
@@ -8,8 +8,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageViewLogin;
+    ImageView imageViewSignUp;
 
 
 
@@ -18,14 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    imageViewLogin=findViewById(R.id.imageViewLogin2);
+    imageViewSignUp=findViewById(R.id.imageViewSignUp);
+    imageViewLogin.setVisibility(View.INVISIBLE);
 
     }
 
 
-    public void OnClickLogin(View view) {
 
-               Intent intent =new Intent(MainActivity.this,LogInActivity.class);
+    public void imageViewLoginClick(View view) {
+
+        if (imageViewLogin.getVisibility() == View.INVISIBLE) {
+            imageViewLogin.setVisibility(View.VISIBLE);
+            Intent intent =new Intent(MainActivity.this,LogInActivity.class);
             startActivity(intent);
+
+        }
 
     }
 }
