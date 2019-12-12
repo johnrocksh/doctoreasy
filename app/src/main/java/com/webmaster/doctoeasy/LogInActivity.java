7ed6ac8922e23f75b7c6aaae990f8a150b5f8e9c
John Rock <ivanshevchenko@mail.ru>
2019-12-12 10:41:23 +0200

    umenshil font na knopkah menu

--- app/src/main/java/com/webmaster/doctoeasy/LogInActivity.java
+++ app/src/main/java/com/webmaster/doctoeasy/LogInActivity.java
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
+
+ public Layout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //activity initialization
+        init();
+
+
+
+    }
+
+    private void init() {
+        View myLayout = LayoutInflater.from(this).inflate(R.layout.bottom_menu,null);
+        View layout=(View)myLayout.findViewById(R.id.btnMyAccaunt);
+        layout.setBackgroundResource(R.drawable.bottom_menu_pressed_bg);
     }
 
     public void backArrowClick(View view) {
